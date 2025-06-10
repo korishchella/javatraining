08339d79b4a914b4adf1b9087032813e24e13804
“Rohith” <rohithteja09@gmail.com>
2025-06-09 23:02:01 -0500

    Second Commit

---
+++ projects/java_basics/src/main/java/org/example/Increment.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Increment {
+    public static void main(String[] args){
+        int a =5, b=5, c,d,e,f;
+        c = a++;
+        d = ++a;
+        e = b--;
+        f = --b;
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+
+    }
+}
