697c0dfdefa63cea8949dfa92725ef46ba11aa44
bhargavichowdary509 <bhargavichowdary509@gmail.com>
2025-06-09 21:29:14 -0500

    AddnNumbers

--- projects/java_basics/src/main/java/org/example/AddnNumber.java
+++ projects/java_basics/src/main/java/org/example/AddnNumber.java
@@ -1,4 +1,17 @@
 package org.example;
 
 public class AddnNumber {
-}
+        // main function
+        public static void main(String[] args)
+        {
+            int N = 4;
+            System.out.println("Finding Sum of " + N + " Numbers using Mathematical Formulae");
+
+            // Calculating the sum
+            int sum = (N*(N+1))/2;
+
+            // Printing the result
+            System.out.println("Sum of first " + N
+                    + " Natural Numbers = " + sum);
+        }
+    }
