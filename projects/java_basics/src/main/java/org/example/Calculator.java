22805e5590bba0c3518aa35f3d9f5c9ede0675c8
Varshita <varshita.kaipa@gmail.com>
2025-06-11 19:16:14 +0530

    Questions for today solved

---
+++ projects/java_basics/src/main/java/org/example/Calculator.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Calculator {
+    public static void main(String[] args) {
+    int a = 20;
+    int b = 10;
+
+    System.out.println(a+b);
+    System.out.println(a-b);
+    System.out.println(a*b);
+    System.out.println(a/b);
+
+    }
+}
