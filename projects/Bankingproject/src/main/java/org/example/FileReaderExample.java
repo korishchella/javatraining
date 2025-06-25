ab51e5ab791b5e523c041ccd2b1ea6349c1b9407
bhargavichowdary509 <bhargavichowdary509@gmail.com>
2025-06-24 22:24:40 -0500

    filereader

--- projects/Bankingproject/src/main/java/org/example/FileReaderExample.java
+++ projects/Bankingproject/src/main/java/org/example/FileReaderExample.java
@@ -1,4 +1,25 @@
 package org.example;
 
-public class FileReader {
-}
+import java.io.File;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+    public class FileReaderExample {
+
+        public static void main(String[] args) {
+            String fileName = "output.txt";
+            try {
+                File myFile = new File(fileName);
+                myFile.createNewFile();
+                BufferedReader reader = new BufferedReader(new FileReader(myFile.getName()));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    System.out.println(line);
+                }
+            } catch (IOException e) {
+                System.err.println("Error reading from file: " + e.getMessage());
+            }
+        }
+    }
+
